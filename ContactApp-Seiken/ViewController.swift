f3ed88a81440601d055433c64a009f288535fe5d
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-03-02 14:18:02 -0800

    Initial Commit

---
+++ ContactApp-Seiken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp-Seiken
+//
+//  Created by Seiken Dojo on 2023-03-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
